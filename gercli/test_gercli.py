6b2cbb1242d2c873a5ee8abb52f38c043abf92fe
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-03-10 20:15:35 +0100

    Refactor
    - Don't use patch_set in thread tuple
    - request for test as a global variable

--- gercli/test_gercli.py
+++ gercli/test_gercli.py
@@ -3,82 +3,100 @@ import threads
 from collections import namedtuple, deque
 
 Comment = namedtuple('Comment', ['patch_set', 'date', 'author', 'parent', 'message'])
-Thread = namedtuple('Thread', ['filename', 'patch_set', 'comments'])
+Thread = namedtuple('Thread', ['filename', 'comments'])
+
+request = {
+        'file.py': [
+            {
+                "patch_set": 1,
+                "id": "TvcXrmjM",
+                "line": 23,
+                "message": "[nit] trailing whitespace",
+                "updated": "2013-02-26 15:40:43.986000000",
+                "author": {
+                    "_account_id": 1000096,
+                    "name": "John Doe",
+                    "email": "john.doe@example.com"
+                    }
+                },
+            {
+                "patch_set": 2,
+                "id": "TfYX-Iuo",
+                "line": 49,
+                "message": "Can you fix this?",
+                "updated": "2013-02-26 15:40:45.328000000",
+                "author": {
+                    "_account_id": 1000097,
+                    "name": "John Doe",
+                    "email": "john.doe@example.com"
+                    }
+                },
+            {
+                "patch_set": 2,
+                "id": "TveXwFiA",
+                "line": 49,
+                "in_reply_to": "TfYX-Iuo",
+                "message": "Done",
+                "updated": "2013-02-26 15:40:45.328000000",
+                "author": {
+                    "_account_id": 1000097,
+                    "name": "Jane Roe",
+                    "email": "jane.roe@example.com"
+                    }
+                }
+            ],
+        'file2.py': [
+            {
+                "patch_set": 1,
+                "id": "sdfh",
+                "line": 49,
+                "message": "What does this mean?",
+                "updated": "2013-02-26 15:40:45.328000000",
+                "author": {
+                    "_account_id": 1000097,
+                    "name": "Jane Roe",
+                    "email": "jane.roe@example.com"
+                    }
+                },
+            {
+                "patch_set": 1,
+                "id": "sdfasd",
+                "line": 49,
+                "in_reply_to": "sdfh",
+                "message": "I should clarify that",
+                "updated": "2013-02-27 15:54:45.328000000",
+                "author": {
+                    "_account_id": 1000097,
+                    "name": "John Doe",
+                    "email": "jane.roe@example.com"
+                    }
+                }
+            ]
+        }
 
 class TestThreads(unittest.TestCase):
 
     def test_create_comment_dict(self):
-        comment_list = [
-                {
-                    "patch_set": 1,
-                    "id": "TvcXrmjM",
-                    "line": 23,
-                    "message": "[nit] trailing whitespace",
-                    "updated": "2013-02-26 15:40:43.986000000",
-                    "author": {
-                        "_account_id": 1000096,
-                        "name": "John Doe",
-                        "email": "john.doe@example.com"
-                        }
-                    },
-                {
-                    "patch_set": 2,
-                    "id": "TveXwFiA",
-                    "line": 49,
-                    "in_reply_to": "TfYX-Iuo",
-                    "message": "Done",
-                    "updated": "2013-02-26 15:40:45.328000000",
-                    "author": {
-                        "_account_id": 1000097,
-                        "name": "Jane Roe",
-                        "email": "jane.roe@example.com"
-                        }
-                    }
-                ]
+        comment_list = request['file.py']
         actual = threads.create_comment_dict(comment_list)
-        expected = {
-                'TvcXrmjM':
-                Comment(1, '2013-02-26 15:40:43', 'John Doe',
-                    None, '[nit] trailing whitespace'),
-                'TveXwFiA': 
-                Comment(2, '2013-02-26 15:40:45', 'Jane Roe', 'TfYX-Iuo', 'Done')
-                }
+        expected = {c['id']: c for c in comment_list}
         self.assertEqual(actual, expected)
 
     def test_find_last_comments(self):
-        comments = {
-                'TvcXrmjM':
-                Comment(1, '2013-02-26 15:40:43', 'John Doe',
-                    None, '[nit] trailing whitespace'),
-                'TfYX-Iuo': 
-                Comment(2, '2013-02-26 15:40:45', 'John Doe',
-                    None, 'Could you fix this?'),
-                'TveXwFiA': 
-                Comment(2, '2013-02-26 15:40:45', 'Jane Roe',
-                    'TfYX-Iuo', 'Done')
-                }
+        comment_list = request['file.py']
+        comments = {c['id']: c for c in comment_list}
         actual = threads.find_last_comments(comments)
-        expected = {comments['TvcXrmjM'], comments['TveXwFiA']}
-        self.assertEqual(actual, expected)
+        expected = [comments['TvcXrmjM'], comments['TveXwFiA']]
+        for t_actual in actual:
+            self.assertIn(t_actual, expected)
 
     def test_create_file_threads(self):
-        comments = {
-                'TvcXrmjM':
-                Comment(1, '2013-02-26 15:40:43', 'John Doe',
-                    None, '[nit] trailing whitespace'),
-                'TfYX-Iuo': 
-                Comment(2, '2013-02-26 15:40:45', 'John Doe',
-                    None, 'Could you fix this?'),
-                'TveXwFiA': 
-                Comment(2, '2013-02-26 15:40:45', 'Jane Roe',
-                    'TfYX-Iuo', 'Done')
-                }
+        comment_list = request['file.py']
+        comments = {c['id']: c for c in comment_list}
         filename = 'file.py'
         expected = [
-                Thread(filename, 1, deque([comments['TvcXrmjM']])),
-                Thread(filename, 2,
-                    deque([comments['TfYX-Iuo'], comments['TveXwFiA']])
-                    ),
+                Thread(filename, deque([comments['TvcXrmjM']])),
+                Thread(filename, deque([comments['TfYX-Iuo'], comments['TveXwFiA']])),
                 ]
         actual = threads.create_file_threads(filename, comments)
         self.assertEqual(len(actual), len(expected))
@@ -86,98 +104,11 @@ class TestThreads(unittest.TestCase):
             self.assertIn(t_actual, expected)
 
     def test_create_threads_from_request(self):
-        request = {
-                'file.py': [
-                    {
-                        "patch_set": 1,
-                        "id": "TvcXrmjM",
-                        "line": 23,
-                        "message": "[nit] trailing whitespace",
-                        "updated": "2013-02-26 15:40:43.986000000",
-                        "author": {
-                            "_account_id": 1000096,
-                            "name": "John Doe",
-                            "email": "john.doe@example.com"
-                            }
-                        },
-                    {
-                        "patch_set": 2,
-                        "id": "TfYX-Iuo",
-                        "line": 49,
-                        "message": "Can you fix this?",
-                        "updated": "2013-02-26 15:40:45.328000000",
-                        "author": {
-                            "_account_id": 1000097,
-                            "name": "John Doe",
-                            "email": "john.doe@example.com"
-                            }
-                        },
-                    {
-                        "patch_set": 2,
-                        "id": "TveXwFiA",
-                        "line": 49,
-                        "in_reply_to": "TfYX-Iuo",
-                        "message": "Done",
-                        "updated": "2013-02-26 15:40:45.328000000",
-                        "author": {
-                            "_account_id": 1000097,
-                            "name": "Jane Roe",
-                            "email": "jane.roe@example.com"
-                            }
-                        }
-                    ],
-                'file2.py': [
-                    {
-                        "patch_set": 1,
-                        "id": "sdfh",
-                        "line": 49,
-                        "message": "What does this mean?",
-                        "updated": "2013-02-26 15:40:45.328000000",
-                        "author": {
-                            "_account_id": 1000097,
-                            "name": "Jane Roe",
-                            "email": "jane.roe@example.com"
-                            }
-                        },
-                    {
-                        "patch_set": 1,
-                        "id": "sdfasd",
-                        "line": 49,
-                        "in_reply_to": "sdfh",
-                        "message": "I should clarify that",
-                        "updated": "2013-02-26 15:54:45.328000000",
-                        "author": {
-                            "_account_id": 1000097,
-                            "name": "John Doe",
-                            "email": "jane.roe@example.com"
-                            }
-                        }
-                    ]
-                }
         actual = threads.create_threads_from_request(request)
         expected = [
-                Thread('file.py', 1,
-                    deque([
-                        Comment(1, '2013-02-26 15:40:43', 'John Doe', None,
-                            '[nit] trailing whitespace')
-                        ])
-                    ),
-                Thread('file.py', 2,
-                    deque([
-                        Comment(2, '2013-02-26 15:40:45', 'John Doe', None,
-                            'Can you fix this?'),
-                        Comment(2, '2013-02-26 15:40:45', 'Jane Roe',
-                            'TfYX-Iuo', 'Done')
-                        ])
-                    ),
-                Thread('file2.py', 1,
-                    deque([
-                        Comment(1, '2013-02-26 15:40:45', 'Jane Roe', None,
-                            'What does this mean?'),
-                        Comment(1, '2013-02-26 15:54:45', 'John Doe', 'sdfh',
-                            'I should clarify that')
-                        ])
-                    ),
+                Thread('file.py', deque([request['file.py'][0]])),
+                Thread('file.py', deque(request['file.py'][1:])),
+                Thread('file2.py', deque(request['file2.py'])),
                 ]
         self.assertEqual(len(actual), len(expected))
         for t_actual in actual:
@@ -185,93 +116,54 @@ class TestThreads(unittest.TestCase):
 
     def test_filter_threads_done(self):
         ts = [
-                Thread('file.py', 2,
-                    deque([
-                        Comment(2, '2013-02-26 15:40:45', 'John Doe', None,
-                            'Can you fix this?'),
-                        Comment(2, '2013-02-26 15:40:45', 'Jane Roe',
-                            'TfYX-Iuo', 'Done')
-                        ])
-                    ),
-                Thread('file.py', 1,
-                    deque([
-                        Comment(1, '2013-02-26 15:40:45', 'Jane Roe', None,
-                            'What does this mean?'),
-                        Comment(1, '2013-02-26 15:54:45', 'John Doe', 'sdfh',
-                            'I should clarify that')
-                        ]),
-                    ),
+                Thread('file.py', deque([request['file.py'][0]])),
+                Thread('file.py', deque(request['file.py'][1:])),
+                Thread('file2.py', deque(request['file2.py'])),
                 ]
         Args = namedtuple('Args', ['done', 'not_done', 'patch_set'])
         args = Args(True, False, None)
         actual = threads.filter_threads(ts, args)
-        expected = [ts[0]]
+        expected = [ts[1]]
         self.assertEqual(actual, expected)
 
     def test_filter_threads_not_done(self):
         ts = [
-                Thread('file.py', 2,
-                    deque([
-                        Comment(2, '2013-02-26 15:40:45', 'John Doe', None,
-                            'Can you fix this?'),
-                        Comment(2, '2013-02-26 15:40:45', 'Jane Roe',
-                            'TfYX-Iuo', 'Done')
-                        ])
-                    ),
-                Thread('file.py', 1,
-                    deque([
-                        Comment(1, '2013-02-26 15:40:45', 'Jane Roe', None,
-                            'What does this mean?'),
-                        Comment(1, '2013-02-26 15:54:45', 'John Doe', 'sdfh',
-                            'I should clarify that')
-                        ]),
-                    ),
+                Thread('file.py', deque([request['file.py'][0]])),
+                Thread('file.py', deque(request['file.py'][1:])),
+                Thread('file2.py', deque(request['file2.py'])),
                 ]
         Args = namedtuple('Args', ['done', 'not_done', 'patch_set'])
         args = Args(False, True, None)
         actual = threads.filter_threads(ts, args)
-        expected = [ts[1]]
-        self.assertEqual(actual, expected)
+        expected = [ts[0]] + [ts[2]]
+        self.assertEqual(len(actual), len(expected))
+        for t_actual in actual:
+            self.assertIn(t_actual, expected)
 
     def test_filter_threads_patch_set(self):
         ts = [
-                Thread('file.py', 2,
-                    deque([
-                        Comment(2, '2013-02-26 15:40:45', 'John Doe', None,
-                            'Can you fix this?'),
-                        Comment(2, '2013-02-26 15:40:45', 'Jane Roe',
-                            'TfYX-Iuo', 'Done')
-                        ])
-                    ),
-                Thread('file.py', 1,
-                    deque([
-                        Comment(1, '2013-02-26 15:40:45', 'Jane Roe', None,
-                            'What does this mean?'),
-                        Comment(1, '2013-02-26 15:54:45', 'John Doe', 'sdfh',
-                            'I should clarify that')
-                        ]),
-                    ),
+                Thread('file.py', deque([request['file.py'][0]])),
+                Thread('file.py', deque(request['file.py'][1:])),
+                Thread('file2.py', deque(request['file2.py'])),
                 ]
         Args = namedtuple('Args', ['done', 'not_done', 'patch_set'])
         args = Args(False, False, 1)
         actual = threads.filter_threads(ts, args)
-        expected = [ts[1]]
-        self.assertEqual(actual, expected)
+        expected = [ts[0]] + [ts[2]]
+        self.assertEqual(len(actual), len(expected))
+        for t_actual in actual:
+            self.assertIn(t_actual, expected)
 
     def test_create_thread_output(self):
-        t = Thread('file.py', 2,
-                deque([
-                    Comment(2, '2013-02-26 15:40:45', 'John Doe', None,
-                        'Can you fix this?'),
-                    Comment(2, '2013-02-26 15:49:45', 'Jane Roe',
-                        'TfYX-Iuo', 'Sure, like this?'),
-                    Comment(2, '2013-02-27 10:40:45', 'Jane Roe',
-                        'asdfsadf', 'Done')
-                    ])
-                )
-        actual = threads.create_thread_output(t)
-        print()
-        print(actual)
+        ts = [
+                Thread('file.py', deque([request['file.py'][0]])),
+                Thread('file.py', deque(request['file.py'][1:])),
+                Thread('file2.py', deque(request['file2.py'])),
+                ]
+        for t in ts:
+            actual = threads.create_thread_output(t)
+            print()
+            print(actual)
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=0)
--- gercli/threads.py
+++ gercli/threads.py
@@ -1,23 +1,33 @@
+import base64
 from collections import namedtuple, deque
 import output
 import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-Comment = namedtuple('Comment', ['patch_set', 'date', 'author', 'parent', 'message'])
-Thread = namedtuple('Thread', ['filename', 'patch_set', 'comments'])
+Thread = namedtuple('Thread', ['filename', 'comments'])
 
 def main(args):
     request = make_request(args)
     threads = create_threads_from_request(request)
 
-def make_request(args):
-    # url = args.server + '/changes/' + str(args.change_id) + '/comments'
-    # r = requests.get(url, HTTPBasicAuth(args.user, args.password))
-    # text = r.text[len(")]}'"):]
-    # return json.loads(text)
-    url = 'https://sv.wikipedia.org/wiki/Portal:Huvudsida'
-    return requests.get(url)
+def comment_request(args):
+    url = args.server + '/changes/' + str(args.change_id) + '/comments'
+    return make_json_request(url, args)
+
+def file_content_request(args):
+    # TODO
+    url = args.server + '/changes/' + str(args.change_id) + '/revision/'
+    pass
+    
+def make_json_request(url, args):
+    r = requests.get(url, HTTPBasicAuth(args.user, args.password))
+    text = r.text[len(")]}'"):]
+    return json.loads(text)
+
+def make_base64_request(url, args):
+    r = requests.get(url, HTTPBasicAuth(args.user, args.password))
+    return base64.standard_b64decode(r.text)
 
 def create_threads_from_request(request):
     threads = []
@@ -28,58 +38,49 @@ def create_threads_from_request(request):
     return threads
 
 def create_comment_dict(comment_list):
-    comment_dict = {}
-    for c in comment_list:
-        comment_dict[c['id']] = Comment(
-                c['patch_set'],
-                c['updated'][:19],
-                c['author']['name'],
-                c.get('in_reply_to', None),
-                c['message']
-                )
-    return comment_dict
+    return {c['id']: c for c in comment_list}
 
 def create_file_threads(filename, comments):
 
     def create_thread(last_comment):
         thread_comments = deque([last_comment])
         c = last_comment
-        while c.parent:
-            c = comments[c.parent]
+        while 'in_reply_to' in c:
+            c = comments[c['in_reply_to']]
             thread_comments.appendleft(c)
-        patch_set = thread_comments[0].patch_set
-        return Thread(filename, patch_set, thread_comments)
+        return Thread(filename, thread_comments)
 
     last_comments = find_last_comments(comments)
     return list(map(create_thread, last_comments))
 
 def find_last_comments(comments):
-    replied_to_comment_ids = {c.parent for c in comments.values() if c.parent}
+    replied_to_comment_ids = {c['in_reply_to'] for c in comments.values() if 'in_reply_to' in c}
     all_ids = set(comments.keys())
     last_comment_ids = all_ids.difference(replied_to_comment_ids)
-    return {comments[c_id] for c_id in last_comment_ids}
+    return [comments[c_id] for c_id in last_comment_ids]
 
 def filter_threads(threads, args):
     if args.done:
-        threads = filter(lambda t: t.comments[-1].message == 'Done', threads)
+        threads = filter(lambda t: t.comments[-1]['message'] == 'Done', threads)
     if args.not_done:
-        threads = filter(lambda t: t.comments[-1].message != 'Done', threads)
+        threads = filter(lambda t: t.comments[-1]['message'] != 'Done', threads)
     if args.patch_set:
-        threads = filter(lambda t: t.patch_set == args.patch_set, threads)
+        threads = filter(lambda t: t.comments[0]['patch_set'] == args.patch_set, threads)
     return list(threads)
 
 def create_thread_output(thread):
     indent = ' ' * 4
     thread_str = []
-    patch_set_str = output.Yellow + 'PS' + str(thread.patch_set) + ' ' + output.END
+    patch_set_str = output.Yellow + 'PS' + str(thread.comments[0]['patch_set']) + ' ' + output.END
     filename_str = output.Yellow + thread.filename + output.END
     thread_str.append(patch_set_str + filename_str)
     date = ''
     for c in thread.comments:
-        if c.date[:10] != date:
-            date = c.date[:10]
-            thread_str.append(indent + date)
-        message_str = output.Italic + c.message + output.END
-        comment_str = indent + c.author + ': ' + message_str
+        if c['updated'][:10] != date:
+            date = c['updated'][:10]
+            date_str = output.Blue + date + output.END
+            thread_str.append(indent + date_str)
+        message_str = output.Italic + c['message'] + output.END
+        comment_str = indent + c['author']['name'] + ': ' + message_str
         thread_str.append(comment_str)
     return '\n'.join(thread_str)
