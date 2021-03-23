0f44deb98887c38b6efb3bc182dfc529ecc9cd3b
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-03-23 19:33:14 +0100

    Move request logic to gerritrequests

--- gercli/threads.py
+++ gercli/threads.py
@@ -1,35 +1,18 @@
 from collections import namedtuple, deque
-import base64
-import json
-import requests
-from requests.auth import HTTPBasicAuth
+import gerritrequests
 import output
 
 Thread = namedtuple('Thread', ['filename', 'comments'])
 
 def main(args):
-    request = comment_request(args)
-    threads = create_threads_from_request(request)
+    requester = gerritrequests.GerritRequester(args.user, args.password, args.server)
+    request = requester.comment_request(args.change_id, args.patch_set)
+    threads = create_threads_from_request(args, requester, request)
+    threads = filter_threads(threads, args)
+    threads = sort_threads(threads, args)
+    output.print_threads(threads, args.stat)
 
-def comment_request(args):
-    url = args.server + '/changes/' + str(args.change_id) + '/comments'
-    return make_json_request(url, args)
-
-def file_content_request(args):
-    # TODO
-    url = args.server + '/changes/' + str(args.change_id) + '/revision/'
-    pass
-
-def make_json_request(url, args):
-    r = requests.get(url, HTTPBasicAuth(args.user, args.password))
-    text = r.text[len(")]}'"):]
-    return json.loads(text)
-
-def make_base64_request(url, args):
-    r = requests.get(url, HTTPBasicAuth(args.user, args.password))
-    return base64.standard_b64decode(r.text)
-
-def create_threads_from_request(request):
+def create_threads_from_request(args, requester, request):
     threads = []
     for filename, comment_list in request.items():
         comments = create_comment_dict(comment_list)
@@ -67,19 +50,8 @@ def filter_threads(threads, args):
         threads = (t for t in threads if t.comments[0]['patch_set'] == args.patch_set)
     return list(threads)
 
-def create_thread_output(thread):
-    indent = ' ' * 4
-    thread_str = []
-    patch_set_str = output.YELLOW + 'PS' + str(thread.comments[0]['patch_set']) + ' ' + output.END
-    filename_str = output.YELLOW + thread.filename + output.END
-    thread_str.append(patch_set_str + filename_str)
-    date = ''
-    for c in thread.comments:
-        if c['updated'][:10] != date:
-            date = c['updated'][:10]
-            date_str = output.BLUE + date + output.END
-            thread_str.append(indent + date_str)
-        message_str = output.ITALIC + c['message'] + output.END
-        comment_str = indent + c['author']['name'] + ': ' + message_str
-        thread_str.append(comment_str)
-    return '\n'.join(thread_str)
+def sort_threads(threads, args):
+    if args.patch_set is None:
+        return sorted(threads, key=lambda thread: thread.comments[0]['patch_set'])
+    else:
+        return threads
